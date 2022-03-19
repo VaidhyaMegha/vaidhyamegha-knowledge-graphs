859aa78ebbfef4ed39680e320b23d7d6f0eb7b8a
Sandeep Kunkunuru <sandeep@vaidhyamegha.com>
2022-03-19 16:02:33 +0530

    Fix : After clearing database content, have to reopen the repository before loading can start

--- src/main/bash/load_data_neptune.sh
+++ src/main/bash/load_data_neptune.sh
@@ -8,12 +8,12 @@ cd temp_nt
 split -l100000 /data/vaidhyamegha_open_kg_clinical_trials.nt
 echo "open knowledge-graph-1" > ../commands.txt
 echo "clear" > ../commands.txt
-
+echo "open knowledge-graph-1" > ../commands.txt
 
 ls x* | while read f; do
 
 	echo "load /data/tools/eclipse-rdf4j-3.6.3/temp_nt/${f}.nt" >> ../commands.txt
-	mv "${f}" "${f}.nt";
+	mv "${f}" "${f}.nt"; 
 
 done;
 
